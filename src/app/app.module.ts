aa76b986c622f2eba882aa262aeec56076fb0bfd
guilherme.dasilva <guilherme.dasilva@radixeng.com.br>
2021-01-04 22:45:39 -0300

    primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent
   ],
   imports: [
     BrowserModule,
